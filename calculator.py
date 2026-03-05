33b4758659358ff94eefbdb7fd9911f843815dee
danh03042008 <trancongdanh03042004@gmail.com>
2026-03-05 22:28:55 +0200

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,16 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError("cant divide with 0")
+    return a / b
+
+def clear():
+    return 0
